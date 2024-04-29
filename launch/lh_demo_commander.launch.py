4f819baace533f228aa66acbbed5913f5e7ef216
Alfred Thoft <alfchr21@student.aau.dk>
2024-04-29 08:50:04 +0200

    speed limit

--- launch/lh_demo_commander.launch.py
+++ launch/lh_demo_commander.launch.py
@@ -6,13 +6,18 @@ from launch_ros.actions import Node
 def generate_launch_description():
     # Pass keypoints as pairs of x and y coordinates
     coordinates_param = DeclareLaunchArgument("keypoints", default_value="[-1.764949,  -0.258354891]")
-    initial_pose_param = DeclareLaunchArgument("initial_pose", default_value="[-3.627925635,  -2.3089, 0.31076, 0.95048564]") # x, y, rz, rw
-    goal_pose_param = DeclareLaunchArgument("goal_pose", default_value="[ -1.30540, 0.16398567, 0.31076, 0.95048564]")
-    
+    # initial_pose_param = DeclareLaunchArgument("initial_pose", default_value="[-3.627925635,  -2.3089, 0.31076, 0.95048564]") # x, y, rz, rw
+    # goal_pose_param = DeclareLaunchArgument("goal_pose", default_value="[ -1.30540, 0.16398567, 0.31076, 0.95048564]")
+    initial_pose_param = DeclareLaunchArgument("initial_pose", default_value="[10.0,  10.0, 0.0, 1.0]") # x, y, rz, rw
+       
+    goal_pose_param = DeclareLaunchArgument("goal_pose", default_value="[ 14.0, 10.0, 0.0, 1.0]")
+
+    speed_limit_param = DeclareLaunchArgument("speed_limit", default_value="0.1")
     return LaunchDescription([
 		coordinates_param,
         initial_pose_param,
         goal_pose_param,
+        speed_limit_param,
 		Node(
 			package="lh_demo_commander",
 			executable="keypoint_estimator",
@@ -23,6 +28,8 @@ def generate_launch_description():
 			package="lh_demo_commander",
 			executable="commander",
 			output="screen",
-            parameters=[{"initial_pose": LaunchConfiguration("initial_pose"), "goal_pose": LaunchConfiguration("goal_pose")}]
+            parameters=[{"initial_pose": LaunchConfiguration("initial_pose"), 
+                         "goal_pose": LaunchConfiguration("goal_pose"), 
+                         "speed_limit": LaunchConfiguration("speed_limit")}]
         ),
 	])
--- lh_demo_commander/commander.py
+++ lh_demo_commander/commander.py
@@ -3,16 +3,20 @@ from rclpy.logging import initialize
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
 from nav2_simple_commander.robot_navigator import BasicNavigator, NavigateToPose
-
+from nav2_msgs.msg import SpeedLimit
 
 class Commander(BasicNavigator):
     def __init__(self, node_name='basic_navigator', namespace=''):
         super().__init__(node_name, namespace)
         self.declare_parameter("initial_pose", rclpy.Parameter.Type.DOUBLE_ARRAY)    
         self.declare_parameter("goal_pose", rclpy.Parameter.Type.DOUBLE_ARRAY)
+        self.declare_parameter("speed_limit", rclpy.Parameter.Type.DOUBLE)
+        
+        self.speed_limit_pub_ = self.create_publisher(msg_type=SpeedLimit, topic="speed_limit", qos_profile=10)
 
         initial_pose = self.get_parameter("initial_pose").value
         goal_pose = self.get_parameter("goal_pose").value
+        self.speed_limit = self.get_parameter("speed_limit").value
 
         self.initial_pose = PoseStamped()
         self.goal_pose = PoseStamped()
@@ -31,10 +35,21 @@ class Commander(BasicNavigator):
         self.setInitialPose(self.initial_pose)
 
         self.waitUntilNav2Active()
+        self.speed_limit_pub()
         
         self.goal_pose.header.frame_id = "map"
         self.goal_pose.header.stamp = self.get_clock().now().to_msg()
         self.goToPose(self.goal_pose)
+    
+    def speed_limit_pub(self):
+        sl = SpeedLimit()
+        sl.header.frame_id = "map" #????
+        sl.header.stamp = self.get_clock().now().to_msg()
+        sl.percentage = False
+        sl.speed_limit = self.speed_limit
+        self.speed_limit_pub_.publish(sl)
+
+        
 
 def main(args: list = None) -> None:
     rclpy.init(args=args)
