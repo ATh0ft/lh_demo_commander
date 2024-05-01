d5f86c3da94a9fcb28a55b5191c58b508f90678a
Alfred Thoft <alfchr21@student.aau.dk>
2024-05-01 21:54:03 +0200

    correct launch params

--- launch/lh_demo_commander.launch.py
+++ launch/lh_demo_commander.launch.py
@@ -5,15 +5,15 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     # Pass keypoints as pairs of x and y coordinates
-    # coordinates_param = DeclareLaunchArgument("keypoints", default_value="[0.934, 2.275]")
-    coordinates_param = DeclareLaunchArgument("keypoints", default_value="[0.0, 0.0]")
-    # initial_pose_param = DeclareLaunchArgument("initial_pose", default_value="[3.305,  3.362, -0.993, 0.118]") # x, y, rz, rw
-    # goal_pose_param = DeclareLaunchArgument("goal_pose", default_value="[ -0.461, 1.630, -0.974, 0.225]")
-    initial_pose_param = DeclareLaunchArgument("initial_pose", default_value="[3.0,  0.0, 0.0, 1.0]") # x, y, rz, rw
+    coordinates_param = DeclareLaunchArgument("keypoints", default_value="[0.934, 2.275]")
+    # coordinates_param = DeclareLaunchArgument("keypoints", default_value="[0.0, 0.0]")
+    initial_pose_param = DeclareLaunchArgument("initial_pose", default_value="[3.305,  3.362, -0.993, 0.118]") # x, y, rz, rw
+    goal_pose_param = DeclareLaunchArgument("goal_pose", default_value="[ -0.461, 1.630, -0.974, 0.225]")
+    # initial_pose_param = DeclareLaunchArgument("initial_pose", default_value="[3.0,  0.0, 0.0, 1.0]") # x, y, rz, rw
        
-    goal_pose_param = DeclareLaunchArgument("goal_pose", default_value="[-3.0, 0.0, 0.0, 1.0]")
+    # goal_pose_param = DeclareLaunchArgument("goal_pose", default_value="[-3.0, 0.0, 0.0, 1.0]")
 
-    speed_limit_param = DeclareLaunchArgument("speed_limit", default_value="0.1")
+    speed_limit_param = DeclareLaunchArgument("speed_limit", default_value="0.15")
     return LaunchDescription([
 		coordinates_param,
         initial_pose_param,
